f9ae208e8dafa0164f32d0cec19391b3bfb24d71
Alex <alexis.kuypers.ai@gmail.com>
2026-07-04 13:32:41 +0200

    feat: add day 9 secret auction

---
+++ 01-beginner/day-009-secret-auction/secret_auction.py
@@ -0,0 +1,54 @@
+from art import art_secret_auction
+
+
+def ask_secret_auction():
+    name = input(f"What is your name?:\n")
+    while True:
+        try:
+            bid = float(input("What is your bid?: $\n"))
+            break
+        except ValueError:
+            print("Invalid bid. Please enter a number.")
+    return name, bid
+
+def restart_choice():
+    while True:
+        choice = input(f"Are there any other bidders? Type 'yes or 'no'.\n").strip().lower()
+        if choice in ("yes", "no"):
+            break
+    return choice
+
+
+secret_auction = {}
+
+def construct_dict(name, bid):
+     secret_auction[name] = bid
+     return secret_auction
+
+
+def winner_auction(secret_auction):
+    max_bid = 0
+    for name, bid in secret_auction.items():
+        if bid > max_bid:
+            max_bid = bid
+            winner_name = name
+    return winner_name, max_bid
+
+
+def output(name, max_bid):
+    print (f"The winner is {name} with a bid of ${max_bid}")
+
+
+def orchestration():
+    print(art_secret_auction)
+    while True:
+        name, bid = ask_secret_auction()
+        secret_auction = construct_dict(name, bid)
+        restart = restart_choice()
+        if restart == "no":
+            break
+    name, max_bid = winner_auction(secret_auction)
+    output(name, max_bid)
+
+
+orchestration()
